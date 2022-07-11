e31dbee3b5e2134c8313f647f9dd9d88724e6363
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-07-11 12:09:38 +0800

    Create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Login - FHE Portal</title>
+    <link rel="icon" type="image/png" sizes="1024x1021" href="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f">
+    <link rel="icon" type="image/png" sizes="1024x1021" href="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f">
+    <link rel="icon" type="image/png" sizes="1024x1021" href="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f">
+    <link rel="icon" type="image/png" sizes="1024x1021" href="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f">
+    <link rel="icon" type="image/png" sizes="1024x1021" href="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css?h=d50d30b664cee6b8784eaf4f5fe306ef">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/fonts/fontawesome5-overrides.min.css?h=18313f04cea0e078412a028c5361bd4e">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="/assets/css/styles.min.css?h=aa1b003f515770cc07bcd7309e11e4eb">
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="card shadow-lg o-hidden border-0 my-5">
+            <div class="card-body p-0">
+                <div class="p-5">
+                    <form class="user">
+                        <div class="form-group">
+                            <div class="text-center"><img src="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f" style="width: 200px;"></div>
+                        </div>
+                        <div class="form-group">
+                            <div class="text-center">
+                                <h5 class="text-dark mb-4">FREE HIGHER EDUCATION PORTAL</h5>
+                            </div>
+                        </div>
+                        <div class="form-group"><input class="form-control form-control-user" type="text" placeholder="Username"></div>
+                        <div class="form-group"><input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Password" name="password"></div>
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox small">
+                                <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1"><label class="form-check-label custom-control-label" for="formCheck-1">Show Password</label></div>
+                            </div>
+                        </div><a class="btn btn-primary btn-block text-white btn-user" role="button" href="/HEI/dashboard.html">Login</a></form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="/assets/js/script.min.js?h=7e9543cad34689d4300cf7002cd79fbe"></script>
+</body>
+
+</html>
