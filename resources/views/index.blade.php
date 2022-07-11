e658de92f1e50b44e491adaea6abe0561235fe06
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-07-11 14:44:40 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,11 +12,13 @@
     <link rel="icon" type="image/png" sizes="1024x1021" href="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f">
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css?h=d50d30b664cee6b8784eaf4f5fe306ef">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/assets/fonts/fontawesome5-overrides.min.css?h=18313f04cea0e078412a028c5361bd4e">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-    <link rel="stylesheet" href="/assets/css/styles.min.css?h=aa1b003f515770cc07bcd7309e11e4eb">
+    <link rel="stylesheet" type="text/css" href="{{url('css\bootstrap.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{url('css\style.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{url('css\iframe.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{url('fonts\fontawesome-all.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{url('fonts\font-awesome.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{url('fonts\fontawesome5-overrides.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css')}}"/>
 </head>
 
 <body class="bg-gradient-primary">
@@ -26,7 +28,7 @@
                 <div class="p-5">
                     <form class="user">
                         <div class="form-group">
-                            <div class="text-center"><img src="/assets/img/UnifastLogo.png?h=f339b22722987739a9a7611640bf0c1f" style="width: 200px;"></div>
+                            <div class="text-center"><img src="{{url('img\UnifastLogo.png')}}" style="width: 200px;"></div>
                         </div>
                         <div class="form-group">
                             <div class="text-center">
@@ -39,16 +41,21 @@
                             <div class="custom-control custom-checkbox small">
                                 <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1"><label class="form-check-label custom-control-label" for="formCheck-1">Show Password</label></div>
                             </div>
-                        </div><a class="btn btn-primary btn-block text-white btn-user" role="button" href="/HEI/dashboard.html">Login</a></form>
+                        </div><a class="btn btn-primary btn-block text-white btn-user" role="button" href="{{route('dashboard')}}">Login</a></form>
                 </div>
             </div>
         </div>
     </div>
+    <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js')}}"></script>
+    <script type="text/javascript" src="{{url('js\bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('js\jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('js\chart.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('js\bs-init.js')}}"></script>
+    <script type="text/javascript" src="{{url('js\theme.js')}}"></script>
+    <script type="text/javascript" src="{{url('js\showandhide.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-    <script src="/assets/js/script.min.js?h=7e9543cad34689d4300cf7002cd79fbe"></script>
 </body>
 
 </html>
