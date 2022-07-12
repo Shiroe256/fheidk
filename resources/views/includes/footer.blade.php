4920cc5d433db7031a9361216d99f99e7b2092d1
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-07-12 08:48:19 +0800

    Update footer.blade.php

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,6 +5,9 @@
 <script type="text/javascript" src="{{url('js\bs-init.js')}}"></script>
 <script type="text/javascript" src="{{url('js\theme.js')}}"></script>
 <script type="text/javascript" src="{{url('js\showandhide.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+<script type="text/javascript" src="{{url('js\datatables.js')}}"></script>
+<script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js')}}"></script>
+<script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js')}}"></script>
+<script src="{{url('https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{url('https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js')}}"></script>
