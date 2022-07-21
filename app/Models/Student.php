dada9f77502ed68a789e1216b734185d740ba369
migsyyy <m.gelorenzo@gmail.com>
2022-07-21 09:31:10 +0800

    added lines in Student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class Student extends Model{
+class Student extends Model
+{
+
+    protected $table = 'tbl_master_student';
+    protected $primarykey = 'uid';
+    protected $fillable = [
+        'app_id', 'fhe_award_no', 'tes_award_no', 'slp_award_no', 'national_id',
+        'fname', 'mname', 'lname', 'ext_name', 'm_fname', 'm_mname', 'm_lname', 'f_fname', 'f_mname',
+        'f_lname', 'sex', 'lrn', 'birthplace', 'birthdate', 'permanent_street', 'permanent_barangay',
+        'permanent_city', 'permanent_province', 'permanent_zip', 'permanent_district', 'permanent_region',
+        'present_street', 'present_barangay', 'present_city', 'present_province', 'present_zip',
+        'present_district', 'present_region', 'pwd_no', 'contact_no', 'alt_contact_no', 'email',
+        'alt_email', 'nationality', 'ay_graduated'
+    ];
+
+
 
     public function createStudent($data = array())
     {
@@ -16,22 +31,19 @@ class Student extends Model{
     public function updateStudent($data = array())
     {
         $student = DB::table('tbl_master_student')->where('uid', $data['uid'])
-        ->update($data);
+            ->update($data);
         return $student;
     }
 
     public function getStudent($uid)
     {
-        $student = DB::table('tbl_master_student')->where('uid',$uid)->first();
+        $student = DB::table('tbl_master_student')->where('uid', $uid)->first();
         return $student;
     }
 
     public function searchStudents($fname = '', $mname = '', $lname = '')
     {
-        $students = DB::table('tbl_master_student')->
-        where('fname','like','%' .  $fname . '%')->
-        where('mname','like','%' .  $mname . '%')->
-        where('lname','like','%' .  $lname . '%')->get();
+        $students = DB::table('tbl_master_student')->where('fname', 'like', '%' .  $fname . '%')->where('mname', 'like', '%' .  $mname . '%')->where('lname', 'like', '%' .  $lname . '%')->get();
 
         return $students;
     }
