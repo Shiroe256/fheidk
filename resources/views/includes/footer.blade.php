06c3f983b6d78e5d04b272441adef6d8f5f06616
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2022-07-24 14:49:16 +0800

    Update footer.blade.php

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,13 +1,13 @@
 <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js')}}"></script>
-<script type="text/javascript" src="{{url('js\bootstrap.min.js')}}"></script>
 <script type="text/javascript" src="{{url('js\jquery.min.js')}}"></script>
-<script type="text/javascript" src="{{url('js\chart.min.js')}}"></script>
-<script type="text/javascript" src="{{url('js\bs-init.js')}}"></script>
-<script type="text/javascript" src="{{url('js\theme.js')}}"></script>
-<script type="text/javascript" src="{{url('js\showandhide.js')}}"></script>
-<script type="text/javascript" src="{{url('js\datatables.js')}}"></script>
 <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js')}}"></script>
 <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js')}}"></script>
 <script src="{{url('https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js')}}"></script>
 <script src="{{url('https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js')}}"></script>
+<script type="text/javascript" src="{{url('js\chart.min.js')}}"></script>
+<script type="text/javascript" src="{{url('js\bs-init.js')}}"></script>
+<script type="text/javascript" src="{{url('js\bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{url('js\theme.js')}}"></script>
+<script type="text/javascript" src="{{url('js\showandhide.js')}}"></script>
+<script type="text/javascript" src="{{url('js\datatables.js')}}"></script>
