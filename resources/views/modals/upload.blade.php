64b8849f776fbb6a1d6658eb3a2a1e580b4be420
migsyyy <m.gelorenzo@gmail.com>
2022-07-25 09:22:41 +0800

    *

---
+++ resources/views/modals/upload.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" role="dialog" tabindex="-1" id="mod_upload">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form>
+                <div class="modal-header">
+                    <h6 class="modal-title">UPLOAD FHE TEMPLATE</h6><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
+                    <div class="form-group input-style">
+                        <div class="form-row">
+                            <div class="col">
+                                <div class="form-group"><input type="file" id="upload_template" data-toggle="tooltip" data-bs-tooltip="" title="Select file to upload"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer"><button class="btn btn-light card-button-style" type="button" data-dismiss="modal">Close</button><button id="btn_upload_template" class="btn btn-primary card-button-style" type="button">Upload</button></div>
+            </form>
+        </div>
+    </div>
+</div>
