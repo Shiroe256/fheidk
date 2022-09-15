cd4f11456a20a36383a304ff6fa3968593c08caf
migsyyy <m.gelorenzo@gmail.com>
2022-09-15 16:21:23 +0800

    th3

--- public/js/batchbilling.js
+++ public/js/batchbilling.js
@@ -71,7 +71,7 @@ fileInput.onchange = () => {
 
         let errorctr = 0; //counts error
         var errors = validateFields(output); //storefields to validate
-        let errorhtml = "<table style='text-align: left; vertical-align:top'><thead><tr><th>Row Number</th><th>  Error Description</th></tr></thead><tbody>";
+        let errorhtml = "<table style='text-align: left; vertical-align:top'><thead><tr><th>Row Number--</th><th>Error Description</th></tr></thead><tbody>";
         let ctr = 1;
         errors.forEach(item => {
             if (item.length > 0) ++errorctr;
