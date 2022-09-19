cff6efa7230d68f8d953d2d775f72901accb227f
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-19 17:39:52 +0800

    Update BillingController.php

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -215,7 +215,7 @@ class BillingController extends Controller
             ->orwhere('remarks', 'Has exceeded the amount of NSTP units.</br>')
             ->orwhere('remarks', 'Has a duplicate this year and semester already</br>')
             ->orwhere('remarks', 'Has a duplicate from other school</br>')
-            ->orwhere('remarks', '</br>Exceeded Maximum Residency with ' . strval($totallength) . ' years</br>')
+            // ->where('remarks', '</br>Exceeded Maximum Residency with ' . strval($totallength) . ' years</br>')
             ->get();
         $output = '';
         if ($exceptions->count() > 0) {
