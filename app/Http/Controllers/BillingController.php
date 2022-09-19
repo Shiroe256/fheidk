4aadc76103092a8a841509c2926f9e63e01cb253
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-19 17:35:31 +0800

    Update BillingController.php

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -210,11 +210,11 @@ class BillingController extends Controller
         $reference_no  = $request->reference_no;
         $exceptions = TemporaryBilling::orderBy('remarks')
             ->where('reference_no', $reference_no)
-            ->where('remarks', 'FHE award no. automatically selected from Master table</br>')
+            // ->where('remarks', 'FHE award no. automatically selected from Master table</br>')
             ->where('remarks', 'Check your spreadsheet. There is a duplicate of this student</br>')
-            ->where('remarks', 'Has exceeded the amount of NSTP units.</br>')
-            ->where('remarks', 'Has a duplicate this year and semester already</br>')
-            ->where('remarks', 'Has a duplicate from other school</br>')
+            // ->where('remarks', 'Has exceeded the amount of NSTP units.</br>')
+            // ->where('remarks', 'Has a duplicate this year and semester already</br>')
+            // ->where('remarks', 'Has a duplicate from other school</br>')
             // ->where('remarks', '</br>Exceeded Maximum Residency with ' . strval($totallength) . ' years</br>')
             ->get();
         $output = '';
