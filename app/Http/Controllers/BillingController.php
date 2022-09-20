6292f58cf62813b103f8e2b858588957069b1277
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-20 17:08:48 +0800

    Update BillingController.php

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -473,11 +473,10 @@ class BillingController extends Controller
         if (is_null($course_enrolled) || empty($course_enrolled) || is_null($year_level) || empty($year_level)) {
             return response()->json(0);
         }else {
-            $otherSchoolFees = SchoolFees::select(DB::raw('reference_no, course_enrolled, year_level, semester, type_of_fee,bs_status , IF(bs_status = 0,0,sum(amount) ) as result'))
-                // ->where('reference_no', $reference_no)
-                // ->where('course_enrolled', $course_enrolled)
-                // ->where('year_level', $year_level)
-                // ->where('semester', $semester)
+            $otherSchoolFees = SchoolFees::where('reference_no', $reference_no)
+                ->where('course_enrolled', $course_enrolled)
+                ->where('year_level', $year_level)
+                ->where('semester', $semester)
                 ->groupby('type_of_fee', 'category')
                 ->get();
             return response()->json($otherSchoolFees);
