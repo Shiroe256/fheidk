1a48227118c0576f983f67bc7baa12b0caa49b65
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-20 11:23:10 +0800

    Update showandhide.js

--- public/js/showandhide.js
+++ public/js/showandhide.js
@@ -14,38 +14,31 @@ $(document).ready(function () {
     //     $("#summary_billing_div").hide(300);
     // }
 
-    if ($('#billing_status').val(3)) {
+    if ($('#billing_status').val() == 3) {
         $("#btn_forms").show(300);
 
         $('#btn_forms').on('click', function (event) {
             $("#btn_forms").hide(300);
             $("#summary_billing_div").hide(300);
             $("#billing_forms_div").show(300);
-            if ($('#billing_status').val(4)) {
-                $("#btn_exceptions").show(300);
-                }
         });
     }
 
-    if ($('#billing_status').val(4)) {
+    if ($('#billing_status').val() == 4) {
         $("#btn_forms").show(300);
 
         $('#btn_forms').on('click', function (event) {
             $("#btn_forms").hide(300);
             $("#billing_exceptions_div").hide(300);
             $("#billing_forms_div").show(300);
-            if ($('#billing_status').val(4)) {
             $("#btn_exceptions").show(300);
-            }
         });
 
         $('#btn_exceptions').on('click', function (event) {
             $("#btn_forms").show(300);
             $("#billing_exceptions_div").show(300);
             $("#billing_forms_div").hide(300);
-            if ($('#billing_status').val(4)) {
-                $("#btn_exceptions").show(300);
-                }
+            $("#btn_exceptions").show(300);
         });
     }
 
