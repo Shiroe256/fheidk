bd758b3a41d72d51b35154bf6a769884d206f0e5
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-20 10:21:54 +0800

    Update showandhide.js

--- public/js/showandhide.js
+++ public/js/showandhide.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
         $("#summary_billing_div").hide(300);
         $("#billing_exceptions_div").hide(300);
     }
-    if($('#summary_billing_div').is(':visible')){
-        $("#billing_forms_div").hide(300);
-        $("#billing_exceptions_div").hide(300);
-    }
-    if($('#billing_exceptions_div').is(':visible')){
-        $("#billing_forms_div").hide(300);
-        $("#summary_billing_div").hide(300);
-    }
+    // if($('#summary_billing_div').is(':visible')){
+    //     $("#billing_forms_div").hide(300);
+    //     $("#billing_exceptions_div").hide(300);
+    // }
+    // if($('#billing_exceptions_div').is(':visible')){
+    //     $("#billing_forms_div").hide(300);
+    //     $("#summary_billing_div").hide(300);
+    // }
 
     $('#btn_forms').on('click', function (event) {
         alert();
