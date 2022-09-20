2b650abc62fe7f1c8cc837a1571ba65be4929325
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-20 10:30:30 +0800

    Update UI

--- public/js/showandhide.js
+++ public/js/showandhide.js
@@ -12,10 +12,6 @@ $(document).ready(function () {
     // if($('#billing_exceptions_div').is(':visible')){
     //     $("#billing_forms_div").hide(300);
     //     $("#summary_billing_div").hide(300);
-    // }
-
-    if($('#billing_status').val(3)){
-        $("#billing_forms_div").show(300);
     }
 
     $('#btn_forms').on('click', function (event) {
