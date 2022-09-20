d73e33d614816163f454e007fb2cacb5323f84fb
migsyyy <m.gelorenzo@gmail.com>
2022-09-20 08:25:26 +0800

    template

--- resources/views/billingmanagement.blade.php
+++ resources/views/billingmanagement.blade.php
@@ -938,6 +938,7 @@
 <script src="{{ url('https://unpkg.com/xlsx/dist/xlsx.full.min.js') }}"></script>
 <script type="text/javascript" src="{{ url('js/batchbilling.js') }}"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js"></script>
 
 
 </body>
