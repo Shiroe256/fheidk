ddbf0d67210f2d78991a85a4eeaad02c0d4c0dd2
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-22 17:10:28 +0800

    Update student_crud.js

--- public/js/student_crud.js
+++ public/js/student_crud.js
@@ -847,7 +847,7 @@ $(document).on('click', 'input[name=checkbox_nstp]', function () {
 $(document).on('change', '#year_level', function () {
   if ($(this).val() == 1) {
     $('.input_transferee').removeClass('d-none')
-  }else if($(this).val() > 1 && $('#checkbox_transferee').checked){
+  }else if($('#checkbox_transferee').checked && $(this).val() > 1){
     $('.input_transferee').removeClass('d-none')
   }else {
     $('.input_transferee').addClass('d-none')
