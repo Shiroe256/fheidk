660542063c9e8a00122cb5c66bc6623dec063dca
migsyyy <m.gelorenzo@gmail.com>
2022-09-22 14:51:13 +0800

    for each nalang

--- public/js/settings.js
+++ public/js/settings.js
@@ -29,7 +29,7 @@ $('[id^="switch_"]').change(function (index) {
 });
 
 function resetCounter(elements) {
-    elements.map(function(element){
+    elements.forEach(element => {
         console.log(element);
         var identifier = element.attr("id").substring(12);
         var count = $('#settings_' + identifier + ' input:checkbox :checked').length;
