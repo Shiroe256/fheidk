f967f35e07b67f121c4e94e5179cbd211d66412f
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-29 09:27:57 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
         <main class="py-4">
                 @yield('content')
         </main>
+</body>
     <script type="text/javascript" src="{{url('js\jquery.min.js')}}"></script>
     <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js')}}"></script>
     <script type="text/javascript" src="{{url('js\chart.min.js')}}"></script>
@@ -23,6 +24,4 @@
     <script type="text/javascript" src="{{url('js\bootstrap.min.js')}}"></script>
     <script type="text/javascript" src="{{url('js\particles\particles.js')}}"></script>
     <script type="text/javascript" src="{{url('js\particles\app.js')}}"></script>
-</body>
-
 </html>
