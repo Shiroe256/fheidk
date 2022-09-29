f019eaa4dc6eda291598d93c345c24d40843624e
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-29 11:38:08 +0800

    Update student_crud.js

--- public/js/student_crud.js
+++ public/js/student_crud.js
@@ -376,14 +376,14 @@ function getOSF() {
           });
         }
 
-        if (computer_fee_per_unit.length >= 0) {
+        if (computer_fee_per_unit.length == 0) {
+          $("#computer_fee_per_unit_div").hide(300);
+        }else{
           $("#computer_fee_per_unit_div").show(300);
           $(document).on('change', '#computer_fee_per_unit', function (e) {
             var total_comp_fee = $("#computer_fee_per_unit").val() * computer_fee_per_unit[0].amount
             $("#computer_fee_per_unit_amount").val(total_comp_fee); 
           });
-        }else{
-          $("#computer_fee_per_unit_div").hide(300);
         }
 
         if (cultural_fee.length == 0) {
