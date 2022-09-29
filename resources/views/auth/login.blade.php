c5275b6a71e09b99882e7d31d81c53232ac4feda
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-29 09:26:17 +0800

    Update UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-
+@section('content')
     <div class="container">
         <div class="card shadow-lg o-hidden border-0 my-5">
             <div class="card-body p-0">
@@ -74,3 +74,5 @@
         </div>
     </div>
 </div>
+
+    @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
         <main class="py-4">
                 @yield('content')
         </main>
-    </div>
     <script type="text/javascript" src="{{url('js\jquery.min.js')}}"></script>
     <script src="{{url('https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js')}}"></script>
     <script type="text/javascript" src="{{url('js\chart.min.js')}}"></script>
