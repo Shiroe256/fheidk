ffe6b5639c2ed8e528c1a2cecf6f50f4e11d1a28
Carlo Molina <cmolina.unifast@ched.gov.ph>
2022-09-29 12:01:11 +0800

    Update summary.js

--- public/js/summary.js
+++ public/js/summary.js
@@ -16,37 +16,37 @@ function fetchTempSummary() {
           "order": [[3, "des"]],
           orderCellsTop: true,
           fixedHeader: true,
-          footerCallback: function (row, data, start, end, display) {
-            var api = this.api();
+        //   footerCallback: function (row, data, start, end, display) {
+        //     var api = this.api();
   
-            // Remove the formatting to get integer data for summation
-            var intVal = function (i) {
-                return typeof i === 'string' ? i.replace(/[\$,]/g, '') * 1 : typeof i === 'number' ? i : 0;
-            };
+        //     // Remove the formatting to get integer data for summation
+        //     var intVal = function (i) {
+        //         return typeof i === 'string' ? i.replace(/[\$,]/g, '') * 1 : typeof i === 'number' ? i : 0;
+        //     };
   
-            // Total over all pages
-            total = api
-                .column(3)
-                .data()
-                .reduce(function (a, b) {
-                    return intVal(a) + intVal(b);
-                }, 0);
+        //     // Total over all pages
+        //     total = api
+        //         .column(3)
+        //         .data()
+        //         .reduce(function (a, b) {
+        //             return intVal(a) + intVal(b);
+        //         }, 0);
   
-            // Update footer
-            $(api.column(3).footer()).html('₱' + total);
+        //     // Update footer
+        //     $(api.column(3).footer()).html('₱' + total);
   
   
-             // Total over all pages
-             total2 = api
-             .column(2)
-             .data()
-             .reduce(function (a, b) {
-                 return intVal(a) + intVal(b);
-             }, 0);
+        //      // Total over all pages
+        //      total2 = api
+        //      .column(2)
+        //      .data()
+        //      .reduce(function (a, b) {
+        //          return intVal(a) + intVal(b);
+        //      }, 0);
   
-         // Update footer
-         $(api.column(2).footer()).html(total2);
-        }
+        //  // Update footer
+        //  $(api.column(2).footer()).html(total2);
+        // }
         });
       }
     });
