7bea661fda1536e18a688024d77977b01f7f7462
migsyyy <m.gelorenzo@gmail.com>
2023-03-21 09:38:30 +0800

    changes to reference number again

--- public/js/student_crud.js
+++ public/js/student_crud.js
@@ -1974,7 +1974,7 @@ $(document).on('click', '#btn_delete_students', function () {
         url: '/delete-tempstudent',
         method: 'delete',
         data: {
-          reference_no: reference_no.val(),
+          reference_no: reference_no.value,
           uid: id
         },
         success: function (response) {
