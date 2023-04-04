fe6fb024a32521baea6f3b2326534d89fb8f8512
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-04-04 09:38:33 +0800

    Update header.blade.php

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/png" sizes="1024x1021" href="assets/img/UnifastLogo.png">
     <link rel="stylesheet" href="{{url('admin\bootstrap\css\bootstrap.min.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
-    <link rel="stylesheet" href="{{url('admin\css\fontawesome-all.min.css')}}">
+    <link rel="stylesheet" href="{{url('admin\fonts\fontawesome-all.min.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
     <link rel="stylesheet" href="{{url('admin\css\style.css')}}">
 </head>
