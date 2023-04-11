8bff5e7d365298113be830a14bd7699e64297efb
migsyyy <m.gelorenzo@gmail.com>
2023-04-11 12:54:10 +0800

    try all

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -941,7 +941,7 @@ class BillingController extends Controller
             //set billing status but not save it yet. IF there are no errors ayun
 
             //get students of each billing transaction
-            $students = TemporaryBilling::where('reference_no','=', $reference_no)->get();
+            $students = TemporaryBilling::all();
             // echo $students;
 
             //check each student in billing transaction for duplciates in fhe award number
