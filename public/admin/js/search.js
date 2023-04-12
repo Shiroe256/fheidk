98434f951357cac8e022759d36b3607a8060c768
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-04-12 18:12:04 +0800

    Update search.js

--- public/admin/js/search.js
+++ public/admin/js/search.js
@@ -5,7 +5,7 @@ function searchBilling() {
     
     // Send AJAX request to the server
     $.ajax({
-      url: "/managebillingsearch",
+      url: "/search",
       type: "POST",
       data: {
         ac_year: acYear,
