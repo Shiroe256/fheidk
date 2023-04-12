b863fe38a25e55c9df0a750c5d7eed79310e9694
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-04-12 17:30:35 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -109,7 +109,7 @@ Route::get('dashboard', 'App\Http\Controllers\AdminController@dashboard')->name(
 Route::get('form1', 'App\Http\Controllers\AdminController@form1')->name('form1');
 Route::get('form2', 'App\Http\Controllers\AdminController@form2')->name('form2');
 Route::get('form3', 'App\Http\Controllers\AdminController@form3')->name('form3');
-// Route::get('managebillinglist', 'App\Http\Controllers\AdminController@managebillinglist')->name('managebillinglist');
+Route::get('managebillinglist', 'App\Http\Controllers\AdminController@managebillinglist')->name('managebillinglist');
 Route::get('managebillingpage', 'App\Http\Controllers\AdminController@managebillingpage')->name('managebillingpage');
 Route::get('manageuserslist', 'App\Http\Controllers\AdminController@manageuserslist')->name('manageuserslist');
 Route::get('manageuserpage', 'App\Http\Controllers\AdminController@manageuserpage')->name('manageuserpage');
