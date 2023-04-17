13082e3650a2510248d75f4090f82ce89e8b2c10
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-04-17 09:58:14 +0800

    Update search.js

--- public/admin/js/search.js
+++ public/admin/js/search.js
@@ -3,6 +3,12 @@ function searchBilling() {
   var semester = document.getElementById("select_semester").value;
   var billingStatus = document.getElementById("select_billing_status").value;
 
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+  
   // Send AJAX request to the server
   $.ajax({
     url: "/admin/search",
@@ -10,8 +16,7 @@ function searchBilling() {
     data: {
       ac_year: acYear,
       semester: semester,
-      billing_status: billingStatus,
-      _token: '{{ csrf_token() }}'
+      billing_status: billingStatus
     },
     success: function (response) {
       alert(3);
