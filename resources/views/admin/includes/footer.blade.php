5fbe5379daa3b68e8faddac420fc6b8e4d986577
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-04-18 09:07:08 +0800

    Update footer.blade.php

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -31,12 +31,12 @@
     </div>
 </footer>
 
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
 <script src="{{url('admin\js\jquery.min.js')}}"></script>
 <script src="{{url('admin\bootstrap\js\bootstrap.min.js')}}"></script>
 <script src="{{url('admin\js\chart.min.js')}}"></script>
 <script src="{{url('admin\js\bs-init.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
 <script src="{{url('admin\js\theme.js')}}"></script>
 <script src="{{url('admin\js\showPassword.js')}}"></script>
 <script src="{{url('admin\js\search.js')}}"></script>
