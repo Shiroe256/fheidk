b1a04be2d466d88291802f7166246c97131ad9b9
migsyyy <m.gelorenzo@gmail.com>
2023-04-24 16:01:05 +0800

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 //CRUD Enrolled
 Route::get('/get-tempstudents', [BillingController::class, 'fetchTempStudent'])->name('fetchAll');
 Route::post('/newtempstudent', [BillingController::class, 'newTempStudent'])->name('newTempStudent')->middleware('validateNewTempStudent');
-Route::post('/add-batchtempstudents', [BillingController::class, 'batchTempStudent'])->middleware('validateTempStudent');
+// Route::post('/add-batchtempstudents', [BillingController::class, 'batchTempStudent'])->middleware('validateTempStudent');
+Route::post('/add-batchtempstudents', [BillingController::class, 'batchTempStudent']);
 Route::get('/edit-tempstudent', [BillingController::class, 'editTempStudent'])->name('editTempStudent');
 Route::post('/update-tempstudent', [BillingController::class, 'updateTempStudent'])->name('updateTempStudent')->middleware('validateUpdateTempStudent');
 Route::delete('/delete-tempstudent', [BillingController::class, 'deleteTempStudent'])->name('deleteTempStudent')->middleware('checkUserHei');
