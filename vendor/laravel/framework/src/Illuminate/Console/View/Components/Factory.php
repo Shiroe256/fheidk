32c616352fa4a2f70db17bef8b48ec6ef3209887
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-01 19:23:03 +0800

    Update Composer

--- vendor/laravel/framework/src/Illuminate/Console/View/Components/Factory.php
+++ vendor/laravel/framework/src/Illuminate/Console/View/Components/Factory.php
@@ -6,14 +6,16 @@ use InvalidArgumentException;
 
 /**
  * @method void alert(string $string, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
+ * @method mixed ask(string $question, string $default = null)
+ * @method mixed askWithCompletion(string $question, array|callable $choices, string $default = null)
  * @method void bulletList(array $elements, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
- * @method mixed choice(string $question, array $choices, $default = null)
- * @method bool confirm(string $question, bool $default = true)
+ * @method mixed choice(string $question, array $choices, $default = null, int $attempts = null, bool $multiple = false)
+ * @method bool confirm(string $question, bool $default = false)
  * @method void error(string $string, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
  * @method void info(string $string, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
  * @method void line(string $style, string $string, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
  * @method void task(string $description, ?callable $task = null, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
- * @method void twoColumnDetail(string $first, ?string $second = null, int $verbosity = OutputInterface::VERBOSITY_NORMAL)
+ * @method void twoColumnDetail(string $first, ?string $second = null, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
  * @method void warn(string $string, int $verbosity = \Symfony\Component\Console\Output\OutputInterface::VERBOSITY_NORMAL)
  */
 class Factory
