f7dcc2c14ffc5f109ccf858271385bbf24b6d228
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-02 17:01:13 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,6 +11,7 @@ use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\OtherSchoolFeesImport;
 use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
@@ -239,48 +240,49 @@ class AdminController extends Controller
         return view('admin.form2', $data);
     }
 
-    public function import(Request $request)
-    {
-        // Load the uploaded file using PHPSpreadsheet
-        $filePath = $request->file('file')->getRealPath();
-        $spreadsheet = IOFactory::load($filePath);
-    
-        // Get the first worksheet of the uploaded file
-        $worksheet = $spreadsheet->getActiveSheet();
-    
-        // Initialize a flag to indicate whether the current row is the header row
-        $isHeaderRow = true;
-    
-        // Loop through the rows of the worksheet and insert the data into the database
-        foreach ($worksheet->getRowIterator() as $row) {
-            if ($isHeaderRow) {
-                // Skip the header row
-                $isHeaderRow = false;
-                continue;
-            }
-    
-            $data = [];
-            foreach ($row->getCellIterator() as $cell) {
-                $data[] = $cell->getValue();
-            }
-    
-            DB::table('tbl_other_school_fees')->insert([
-                'ac_year' => $data[1],
-                'hei_psg_region' => $data[2],
-                'hei_uii' => $data[3],
-                'hei_name' => $data[4],
-                'year_level' => $data[5],
-                'semester' => $data[6],
-                'course_enrolled' => $data[7],
-                'type_of_fee' => $data[8],
-                'category' => $data[9],
-                'coverage' => $data[10],
-                'is_optional' => $data[12],
-                'amount' => $data[11],
-            ]);
-        }
-    
-        return redirect()->back()->with('success', 'File uploaded successfully.');
+public function import(Request $request)
+{
+    // Validate the uploaded file
+    $validator = Validator::make($request->all(), [
+        'file' => 'required|mimes:xlsx,xls,csv'
+    ]);
+
+    // If validation fails, redirect back with errors
+    if ($validator->fails()) {
+        return redirect()->back()->withErrors($validator);
     }
+
+    // Load the uploaded file using PHPSpreadsheet
+    $filePath = $request->file('file')->getRealPath();
+    $spreadsheet = IOFactory::load($filePath);
+
+    // Get the first worksheet of the uploaded file
+    $worksheet = $spreadsheet->getActiveSheet();
+
+    // Loop through the rows of the worksheet and insert the data into the database
+    foreach ($worksheet->getRowIterator() as $row) {
+        $data = [];
+        foreach ($row->getCellIterator() as $cell) {
+            $data[] = $cell->getValue();
+        }
+
+        DB::table('tbl_other_school_fees')->insert([
+            'ac_year' => $data[1],
+            'hei_psg_region' => $data[2],
+            'hei_uii' => $data[3],
+            'hei_name' => $data[4],
+            'year_level' => $data[5],
+            'semester' => $data[6],
+            'course_enrolled' => $data[7],
+            'type_of_fee' => $data[8],
+            'category' => $data[9],
+            'coverage' => $data[10],
+            'is_optional' => $data[12],
+            'amount' => $data[11],
+        ]);
+    }
+
+    return redirect()->back()->with('success', 'File uploaded successfully.');
+}
 
 }
