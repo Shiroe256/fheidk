9f6d1e4a0e61377dc1674ba4013303ae7af67e5b
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-02 13:26:35 +0800

    Update OtherSchoolFeesImport.php

--- app/Imports/OtherSchoolFeesImport.php
+++ app/Imports/OtherSchoolFeesImport.php
@@ -4,7 +4,7 @@ namespace App\Imports;
 use App\Models\OtherSchoolFees;
 use Maatwebsite\Excel\Concerns\ToModel;
 
-require '../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 class OtherSchoolFeesImport implements ToModel
 {
