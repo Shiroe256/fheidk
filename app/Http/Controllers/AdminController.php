671ead0e5ab1653492175a31c21b5f3e3e693951
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-04 09:51:21 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -241,64 +241,65 @@ class AdminController extends Controller
     }
 
     public function import(Request $request)
-{
-    // Validate the uploaded file
-    $request->validate([
-        'file' => 'required|mimes:xlsx,xls,csv'
-    ]);
-
-    // Load the uploaded file using PHPSpreadsheet
-    $filePath = $request->file('file')->getRealPath();
-    $spreadsheet = IOFactory::load($filePath);
-
-    // Get the first worksheet of the uploaded file
-    $worksheet = $spreadsheet->getActiveSheet();
-
-    // Initialize a flag to indicate whether the current row is the header row
-    $isHeaderRow = true;
-
-    // Loop through the rows of the worksheet and insert the data into the database
-    foreach ($worksheet->getRowIterator() as $row) {
-        if ($isHeaderRow) {
-            // Skip the header row
-            $isHeaderRow = false;
-            continue;
-        }
-
-        $data = [];
-        foreach ($row->getCellIterator() as $cell) {
-            $data[] = $cell->getValue();
-        }
-        // Validate the data before creating the record
-        $validator = Validator::make($data, [
-            'year_level' => 'numeric',
-            'semester' => 'numeric',
-            'amount' => 'numeric',
-            'is_optional' => 'numeric|in:0,1',
+    {
+        // Validate the uploaded file
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls,csv'
         ]);
 
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
+        // Load the uploaded file using PHPSpreadsheet
+        $filePath = $request->file('file')->getRealPath();
+        $spreadsheet = IOFactory::load($filePath);
 
-        OtherSchoolFees::create([
-            'ac_year' => $data[0],
-            'hei_psg_region' => $data[1],
-            'hei_uii' => $data[2],
-            'hei_name' => $data[3],
-            'year_level' => $data[4],
-            'semester' => $data[5],
-            'course_enrolled' => $data[6],
-            'type_of_fee' => $data[7],
-            'category' => $data[8],
-            'coverage' => $data[9],
-            'amount' => $data[10],
-            'is_optional' => $data[11],
-        ]);
+        // Get the first worksheet of the uploaded file
+        $worksheet = $spreadsheet->getActiveSheet();
+
+        // Initialize a flag to indicate whether the current row is the header row
+        $isHeaderRow = true;
+
+        // Loop through the rows of the worksheet and insert the data into the database
+        foreach ($worksheet->getRowIterator() as $row) {
+            if ($isHeaderRow) {
+                // Skip the header row
+                $isHeaderRow = false;
+                continue;
+            }
+
+            $data = [];
+            foreach ($row->getCellIterator() as $cell) {
+                $data[] = $cell->getValue();
+            }
+            // // Validate the data before creating the record
+            // $validator = Validator::make($data, [
+            //     'year_level' => 'numeric',
+            //     'semester' => 'numeric',
+            //     'amount' => 'numeric',
+            //     'is_optional' => 'numeric|in:0,1',
+            // ]);
+
+            // if ($validator->fails()) {
+            //     return redirect()->back()->withErrors($validator)->withInput();
+            // }
+
+        //     OtherSchoolFees::create([
+        //         'ac_year' => $data[0],
+        //         'hei_psg_region' => $data[1],
+        //         'hei_uii' => $data[2],
+        //         'hei_name' => $data[3],
+        //         'year_level' => $data[4],
+        //         'semester' => $data[5],
+        //         'course_enrolled' => $data[6],
+        //         'type_of_fee' => $data[7],
+        //         'category' => $data[8],
+        //         'coverage' => $data[9],
+        //         'amount' => $data[10],
+        //         'is_optional' => $data[11],
+        //     ]);
+        // }
+
+        // return redirect()->back()->with('success', 'File uploaded successfully.');
+
+        print_r($data);
+        }
     }
-
-    return redirect()->back()->with('success', 'File uploaded successfully.');
-}
-
-
 }
