dda192b52d2451bc44599fd009431cd58a013377
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-10 16:25:52 +0800

    Update billinglist.js

--- public/admin/js/billinglist.js
+++ public/admin/js/billinglist.js
@@ -41,7 +41,7 @@ function fetchBillingListAdmin() {
 
   function fetchtosflist() {
     $.ajax({
-      url: "/admin/fetchtosflist",
+      url: "/admin/fetchtosflist/{hei_uii}",
       method: 'get',
       data: {
         _token: '{{ csrf_token() }}'
