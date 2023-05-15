1f82e427e8bdb97ffd70777334eeef367f53559d
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-15 15:36:32 +0800

    Update billinglist.js

--- public/admin/js/billinglist.js
+++ public/admin/js/billinglist.js
@@ -50,6 +50,21 @@ function fetchtosflist() {
     },
     success: function (response) {
       $("#tbl_tosf_div").html(response);
+
+      $('#tbl_tosf thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+ 
+        $( 'input', this ).on( 'keyup change', function () {
+            if ( table.column(i).search() !== this.value ) {
+                table
+                    .column(i)
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+
       $("#tbl_tosf").DataTable({
         "order": [[0, "asc"]],
         orderCellsTop: true,
