9f72b4b0b159e34573c477fed280a0a74b0a85eb
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-29 14:55:29 +0800

    Update form2.js

--- public/admin/js/form2.js
+++ public/admin/js/form2.js
@@ -2,25 +2,26 @@ fetchform2list();
 
 //fetch records from the database
 function fetchform2list() {
-  $.ajax({
-    url: "/admin/fetchform2list",
-    method: 'get',
-    data: {
-        reference_no : reference_no,
-        _token: '{{ csrf_token() }}'
-      },
-    success: function (response) {
-      $("#tbl_form2_div").html(response);
+    var reference_no = $("#reference_no").val();
+    $.ajax({
+        url: "/admin/fetchform2list",
+        method: 'get',
+        data: {
+            reference_no: reference_no,
+            _token: '{{ csrf_token() }}'
+        },
+        success: function (response) {
+            $("#tbl_form2_div").html(response);
 
-   $("#tbl_form2").DataTable({
-        "order": [2, "asc"],
-        orderCellsTop: true,
-        fixedHeader: true,
-        'columnDefs': [ {
-          'targets': [8], /* column index */
-          'orderable': false, /* true or false */
-       }]
-      });
-    }
-  });
+            $("#tbl_form2").DataTable({
+                "order": [2, "asc"],
+                orderCellsTop: true,
+                fixedHeader: true,
+                'columnDefs': [{
+                    'targets': [8], /* column index */
+                    'orderable': false, /* true or false */
+                }]
+            });
+        }
+    });
 }
