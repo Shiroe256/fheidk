19a98b0385c7112c45a8f1418782824536094be8
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-05-29 15:20:09 +0800

    Update form2.js

--- public/admin/js/form2.js
+++ public/admin/js/form2.js
@@ -1,48 +1,27 @@
- // Call fetchform2list to fetch and display data
- fetchform2list();
-  
-// Fetch records from the database
+fetchform2list();
+
+//fetch records from the database
 function fetchform2list() {
-    var reference_no = $("#reference_no").val(); // Get the value of reference_no from the input field
-  
-    $("#tbl_form2").DataTable({
-      "processing": true,
-      "serverSide": true,
-      "ajax": {
-        "url": "/admin/fetchform2list",
-        "method": "GET",
-        "data": {
-          "reference_no": reference_no,
-          "_token": '{{ csrf_token() }}'
+    var reference_no = $("#reference_no").val();
+    $.ajax({
+        url: "/admin/fetchform2list",
+        method: 'get',
+        data: {
+            reference_no: reference_no,
+            _token: '{{ csrf_token() }}'
         },
-        "success": function (response) {
-          $("#tbl_form2_div").html(response); // Populate the table container with the HTML response
+        success: function (response) {
+            $("#tbl_form2_div").html(response);
+
+            $("#tbl_form2").DataTable({
+                "order": [2, "asc"],
+                orderCellsTop: true,
+                fixedHeader: true,
+                'columnDefs': [{
+                    'targets': [8], /* column index */
+                    'orderable': false, /* true or false */
+                }]
+            });
         }
-      },
-      "columns": [
-        { "data": "app_id" }, // Column for app_id
-        { "data": "fhe_award_no" }, // Column for fhe_award_no
-        { 
-          "data": function (row) {
-            // Column for concatenation of stud_lname, stud_fname, and stud_mname
-            return row.stud_lname + ', ' + row.stud_fname + ' ' + row.stud_mname;
-          }
-        },
-        { "data": "degree_program" }, // Column for degree_program
-        { "data": "year_level" }, // Column for year_level
-        { "data": "remarks" }, // Column for remarks
-        { "data": "total_fees" }, // Column for total_fees
-        { "data": "stud_status" }, // Column for stud_status
-      ],
-      "order": [2, "asc"],
-      "orderCellsTop": true,
-      "fixedHeader": true,
-      "columnDefs": [{
-        "targets": [8], /* column index */
-        "orderable": false /* true or false */
-      }]
     });
-  }
-  
- 
-  
+}
