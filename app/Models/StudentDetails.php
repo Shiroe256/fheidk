9cf79ffd4925d533a9975f36c84f3a9ce925ed43
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-06-01 15:46:56 +0800

    Update StudentDetails.php

--- app/Models/StudentDetails.php
+++ app/Models/StudentDetails.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class StudentDetails extends Model
 {
     use HasFactory;
-    public $table = "view_user_data";
+    public $table = "vw_student_billing_details";
     protected $primaryKey = 'reference_no';
 }
