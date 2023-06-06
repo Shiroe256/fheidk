0c4fbe0c492bb12bf8fab25bfe3b6872e2ad9536
migsyyy <m.gelorenzo@gmail.com>
2023-06-06 11:37:47 +0800

    added middle name to search

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -53,14 +53,16 @@ class BillingController extends Controller
         $search = $request->search['value'];
         $total = DB::table('tbl_billing_details_temp')->where('tbl_billing_details_temp.reference_no', '=', $reference_no)->where(function ($query) use ($search) {
             $query->where('stud_fname', 'like', '%' . $search . '%')
-                ->orWhere('stud_lname', 'like', '%' . $search . '%');
+                ->orWhere('stud_lname', 'like', '%' . $search . '%')
+                ->orWhere('stud_mname', 'like', '%' . $search . '%');
         })->count();
 
         // $temporary_billing_info = new TemporaryBilling();
         //students sub query. Dito ung pagination
         $students_sub = DB::table('tbl_billing_details_temp')->where('tbl_billing_details_temp.reference_no', '=', $reference_no)->where(function ($query) use ($search) {
             $query->where('stud_fname', 'like', '%' . $search . '%')
-                ->orWhere('stud_lname', 'like', '%' . $search . '%');
+                ->orWhere('stud_lname', 'like', '%' . $search . '%')
+                ->orWhere('stud_mname', 'like', '%' . $search . '%');
         })
             ->skip($request->start)->take($request->length);
         //dito jinojoin ung information about the fees and computation
