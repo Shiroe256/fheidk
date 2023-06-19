a642f7a4a9b0b07d91580c2b96c652d17d01e934
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-06-19 17:48:25 +0800

    Update form2list.blade.php

--- resources/views/admin/elements/form2list.blade.php
+++ resources/views/admin/elements/form2list.blade.php
@@ -18,7 +18,13 @@
             <td>{{ $student->stud_lname . ' ' . $student->stud_fname . ' ' . $student->stud_mname }}</td>
             <td>{{ $student->degree_program }}</td>
             <td class="text-center">{{ $student->year_level }}</td>
-            <td class="text-left">{!! $student->remarks !!}</td>
+            <td class="text-left">
+                @if ($student->remarks == '<span class="badge badge-danger">Duplicate</span>')
+                    <span class="badge badge-danger">Duplicate</span>
+                @else
+                    {{ $student->remarks }}
+                @endif
+            </td>
             <td class="text-right">{{ $student->total_fee }}</td>
             <td>{{ $student->stud_status }}</td>
             <td class="text-center">
