7db23bfec77f611be59e54d42da60935cbc50fe9
migsyyy <m.gelorenzo@gmail.com>
2023-06-22 16:01:23 +0800

    wait baka gumana palaa

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -649,6 +649,7 @@ SUM(
         $applicants = DB::table('tbl_billing_details_temp as students_sub')
             ->select(
                 'students_sub.uid',
+                'students_sub.hei_name',
                 DB::raw($this->carlo_columns),
                 DB::raw('sum(if(tbl_other_school_fees.category = "ENTRANCE OR ADMISSION EXAM", tbl_other_school_fees.amount * students_sub.total_exam_taken, 0)) as exam_fees')
             )
@@ -669,6 +670,7 @@ SUM(
             ->mergeBindings($students_sub)
             ->select(
                 'students_sub.uid',
+                'students_sub.hei_name',
                 DB::raw($this->carlo_columns)
             )
             ->leftJoin('tbl_other_school_fees', function ($join) {
@@ -698,16 +700,16 @@ SUM(
             })
             ->groupBy('students_sub.uid');
 
-        // $union = $applicants->union($students);
-        // $data['hei_summary'] = DB::table(DB::raw("({$union->toSql()}) AS summary"))
-        //     ->mergeBindings($union)
-        //     ->selectRaw('summary.hei_name, COUNT(*) AS total_beneficiaries, sum(summary.total_fee) + sum(summary.exam_fee) as total_amount')
-        //     ->get();
-
-        $data['hei_summary'] = DB::table(DB::raw("({$students->toSql()}) as students"))
-            ->mergeBindings($students)
-            ->selectRaw('students.hei_name, COUNT(*) AS total_beneficiaries, sum(total_fee) as total_amount')
+        $union = $applicants->union($students);
+        $data['hei_summary'] = DB::table(DB::raw("({$union->toSql()}) AS summary"))
+            ->mergeBindings($union)
+            ->selectRaw('summary.hei_name, COUNT(*) AS total_beneficiaries, sum(summary.total_fee) + sum(summary.exam_fee) as total_amount')
             ->get();
+
+        // $data['hei_summary'] = DB::table(DB::raw("({$students->toSql()}) as students"))
+        //     ->mergeBindings($students)
+        //     ->selectRaw('students.hei_name, COUNT(*) AS total_beneficiaries, sum(total_fee) as total_amount')
+        //     ->get();
 
         // $billing_record->total_beneficiaries = $data['hei_summary'][0]->total_beneficiaries;
         // $billing_record->total_amount = $data['hei_summary'][0]->total_amount;
