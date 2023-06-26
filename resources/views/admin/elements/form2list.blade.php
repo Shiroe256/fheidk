3d66dcfeea8c928af24ae150662039147ec578e7
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-06-26 16:35:05 +0800

    Update form2list.blade.php

--- resources/views/admin/elements/form2list.blade.php
+++ resources/views/admin/elements/form2list.blade.php
@@ -20,7 +20,15 @@
             <td class="text-center">{{ $student->year_level }}</td>
             <td class="text-left">{!! $student->remarks !!}</td>
             <td class="text-right">{{ $student->total_fee }}</td>
-            <td>{{ $student->stud_status }}</td>
+            <td>
+                @if ($student->stud_status == 0)
+                    <span class="badge badge-pill badge-success billing-status-badge">Enrolled<br /></span>
+                @elseif ($student->stud_status == 1)
+                    <span class="badge badge-pill badge-warning billing-status-badge">On-LOA<br /></span>
+                @elseif ($student->stud_status == 2)
+                    <span class="badge badge-pill badge-danger billing-status-badge">Dropped<br /></span>
+                @endif
+            </td>
             <td class="text-center">
                 <button id="{{$student->stud_uid}}" class="btn_view_student_info btn btn-outline-info btn-block btn-sm border rounded-pill" type="button" data-toggle="modal" data-target="#mod_student_info">View</button>
             </td>
