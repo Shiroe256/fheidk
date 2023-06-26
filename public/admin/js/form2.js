f623c5d338284afb156e107554bd60f1ed4d20ce
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-06-26 10:33:54 +0800

    Update form2.js

--- public/admin/js/form2.js
+++ public/admin/js/form2.js
@@ -48,6 +48,8 @@ $(document).on('click', '.btn_view_student_info', function (e) {
             $('#stud_lname').val(response.stud_lname);
             $('#stud_fname').val(response.stud_fname);
             $('#stud_mname').val(response.stud_mname);
+            $('#stud_sex').val(response.stud_sex);
+            $('#stud_bdate').val(response.stud_birthdate);
         }
     });
 });
