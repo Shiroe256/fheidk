3d6ac5ad5b3f3e5f2b0d9ce273a22d627ea1b13b
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-07-03 13:12:19 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -433,141 +433,74 @@ class AdminController extends Controller
         return view('admin.manage-users-page', ['heis' => $heis]);
     }
 
-    // public function fetchbillinglist()
-    // {
-    //     $billings =  Billing::leftJoin('vw_billing_details', function ($join) {
-    //         $join->on('tbl_fhe_billing_records.reference_no', '=', 'vw_billing_details.reference_no')
-    //             ->where(function ($query) {
-    //                 $query->where('vw_billing_details.bs_osf_settings', 1)
-    //                     ->orWhere('vw_billing_details.bs_student_osf_settings', 1);
-    //             })
-    //             ->where('vw_billing_details.form', 2);
-    //     })
-    //     ->select(
-    //         'tbl_fhe_billing_records.*',
-    //         DB::raw('COUNT(DISTINCT vw_billing_details.stud_uid) AS total_beneficiaries'),
-    //         DB::raw('SUM(
-    //             CASE
-    //                 WHEN (vw_billing_details.type_of_fee = "tuition" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
-    //                     THEN (vw_billing_details.academic_unit * vw_billing_details.amount)
-    //                 WHEN (vw_billing_details.type_of_fee = "tuition" AND vw_billing_details.coverage = "per student")
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "entrance"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "admission"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "athletic"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN (vw_billing_details.type_of_fee = "computer" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
-    //                     THEN (vw_billing_details.comp_lab_unit * vw_billing_details.amount)
-    //                 WHEN (vw_billing_details.type_of_fee = "computer" AND vw_billing_details.coverage = "per student")
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "cultural"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "development"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "guidance"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "handbook"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN (vw_billing_details.type_of_fee = "laboratory" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
-    //                     THEN (vw_billing_details.lab_unit * vw_billing_details.amount)
-    //                 WHEN (vw_billing_details.type_of_fee = "laboratory" AND vw_billing_details.coverage = "per student")
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "library"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "medical and dental"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "registration"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN vw_billing_details.type_of_fee = "school id"
-    //                     THEN vw_billing_details.amount
-    //                 WHEN (vw_billing_details.type_of_fee = "nstp" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
-    //                     THEN (vw_billing_details.nstp_unit * vw_billing_details.amount)
-    //                 WHEN (vw_billing_details.type_of_fee = "nstp" AND vw_billing_details.coverage = "per student")
-    //                     THEN vw_billing_details.amount
-    //                 ELSE 0
-    //             END
-    //         ) AS total_amount')
-    //     )
-    //     ->groupBy(
-    //         'tbl_fhe_billing_records.reference_no',
-    //     )
-    //     ->get();
+    public function fetchbillinglist()
+    {
+        $billings =  Billing::leftJoin('vw_billing_details', function ($join) {
+            $join->on('tbl_fhe_billing_records.reference_no', '=', 'vw_billing_details.reference_no')
+                ->where(function ($query) {
+                    $query->where('vw_billing_details.bs_osf_settings', 1)
+                        ->orWhere('vw_billing_details.bs_student_osf_settings', 1);
+                })
+                ->where('vw_billing_details.form', 2);
+        })
+        ->select(
+            'tbl_fhe_billing_records.*',
+            DB::raw('COUNT(DISTINCT vw_billing_details.stud_uid) AS total_beneficiaries'),
+            DB::raw('SUM(
+                CASE
+                    WHEN (vw_billing_details.type_of_fee = "tuition" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
+                        THEN (vw_billing_details.academic_unit * vw_billing_details.amount)
+                    WHEN (vw_billing_details.type_of_fee = "tuition" AND vw_billing_details.coverage = "per student")
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "entrance"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "admission"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "athletic"
+                        THEN vw_billing_details.amount
+                    WHEN (vw_billing_details.type_of_fee = "computer" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
+                        THEN (vw_billing_details.comp_lab_unit * vw_billing_details.amount)
+                    WHEN (vw_billing_details.type_of_fee = "computer" AND vw_billing_details.coverage = "per student")
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "cultural"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "development"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "guidance"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "handbook"
+                        THEN vw_billing_details.amount
+                    WHEN (vw_billing_details.type_of_fee = "laboratory" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
+                        THEN (vw_billing_details.lab_unit * vw_billing_details.amount)
+                    WHEN (vw_billing_details.type_of_fee = "laboratory" AND vw_billing_details.coverage = "per student")
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "library"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "medical and dental"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "registration"
+                        THEN vw_billing_details.amount
+                    WHEN vw_billing_details.type_of_fee = "school id"
+                        THEN vw_billing_details.amount
+                    WHEN (vw_billing_details.type_of_fee = "nstp" AND (vw_billing_details.coverage = "per unit" OR vw_billing_details.coverage = "per subject"))
+                        THEN (vw_billing_details.nstp_unit * vw_billing_details.amount)
+                    WHEN (vw_billing_details.type_of_fee = "nstp" AND vw_billing_details.coverage = "per student")
+                        THEN vw_billing_details.amount
+                    ELSE 0
+                END
+            ) AS total_amount')
+        )
+        ->groupBy(
+            'tbl_fhe_billing_records.reference_no',
+        )
+        ->get();
     
 
-    //     // $billings = Billing::all();
+        // $billings = Billing::all();
         
-    //     $data['billings'] = $billings;
-    //     return view('admin.elements.billinglist', $data);
-    // }
-
-    public function fetchbillinglist()
-{
-    $billings = Billing::with('vwBillingDetails')
-        ->select('tbl_fhe_billing_records.*')
-        ->withCount('vwBillingDetails as total_beneficiaries')
-        ->get()
-        ->map(function ($billing) {
-            $billing->total_amount = $this->calculateTotalAmount($billing->vwBillingDetails);
-            return $billing;
-        });
-
-    $data['billings'] = $billings;
-    return view('admin.elements.billinglist', $data);
-}
-
-private function calculateTotalAmount($vwBillingDetails)
-{
-    $feeMapping = [
-        'tuition' => [
-            'per unit' => ['amount' => 'academic_unit', 'multiplier' => 'amount'],
-            'per subject' => ['amount' => 'academic_unit', 'multiplier' => 'amount'],
-            'per student' => ['amount' => 'amount'],
-        ],
-        'entrance' => ['amount' => 'amount'],
-        'admission' => ['amount' => 'amount'],
-        'athletic' => ['amount' => 'amount'],
-        'computer' => [
-            'per unit' => ['amount' => 'comp_lab_unit', 'multiplier' => 'amount'],
-            'per subject' => ['amount' => 'comp_lab_unit', 'multiplier' => 'amount'],
-            'per student' => ['amount' => 'amount'],
-        ],
-        'cultural' => ['amount' => 'amount'],
-        'development' => ['amount' => 'amount'],
-        'guidance' => ['amount' => 'amount'],
-        'handbook' => ['amount' => 'amount'],
-        'laboratory' => [
-            'per unit' => ['amount' => 'lab_unit', 'multiplier' => 'amount'],
-            'per subject' => ['amount' => 'lab_unit', 'multiplier' => 'amount'],
-            'per student' => ['amount' => 'amount'],
-        ],
-        'library' => ['amount' => 'amount'],
-        'medical and dental' => ['amount' => 'amount'],
-        'registration' => ['amount' => 'amount'],
-        'school id' => ['amount' => 'amount'],
-        'nstp' => [
-            'per unit' => ['amount' => 'nstp_unit', 'multiplier' => 'amount'],
-            'per subject' => ['amount' => 'nstp_unit', 'multiplier' => 'amount'],
-            'per student' => ['amount' => 'amount'],
-        ],
-    ];
-
-    $totalAmount = 0;
-
-    foreach ($vwBillingDetails as $detail) {
-        $typeOfFee = $detail->type_of_fee;
-        $coverage = $detail->coverage;
-
-        if (isset($feeMapping[$typeOfFee][$coverage])) {
-            $amount = $detail->{$feeMapping[$typeOfFee][$coverage]['amount']};
-            $multiplier = $feeMapping[$typeOfFee][$coverage]['multiplier'] ?? 1;
-            $totalAmount += $amount * $multiplier;
-        }
+        $data['billings'] = $billings;
+        return view('admin.elements.billinglist', $data);
     }
-
-    return $totalAmount;
-}
 
 
     public function fetchtosflist(Request $request)
