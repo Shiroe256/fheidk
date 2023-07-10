a5b741b9a1fee33a1d218c29a974ecae34845067
Carlo Molina <81338707+CMolina5@users.noreply.github.com>
2023-07-10 15:46:09 +0800

    Update billingmanagement-attachments.blade.php

--- resources/views/billingmanagement-attachments.blade.php
+++ resources/views/billingmanagement-attachments.blade.php
@@ -43,11 +43,17 @@
                                                 Summary</h5>
                                         </div>
                                         <div class="col text-right">
-                                            <div class="dropdown no no-arrow"><button class="btn btn-outline-primary btn-sm" aria-expanded="false" data-toggle="dropdown" type="button"><i class="far fa-file-pdf"></i> Download Generated Forms</button>
+                                            <div class="dropdown no-arrow">
+                                                <button class="btn btn-outline-primary btn-sm dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button">
+                                                  <i class="far fa-file-pdf"></i> Download Generated Forms
+                                                </button>
                                                 <div class="dropdown-menu dropdown-menu-right animated--fade-in">
-                                                    <h6 class="dropdown-header">SELECT FORM TO DOWNLOAD:</h6><a class="dropdown-item" href="#"><i class="fas fa-file-download fa-sm fa-fw mr-2 text-gray-400"></i>Form 1 (Billing Summary)</a><a class="dropdown-item" href="#"><i class="fas fa-file-download fa-sm fa-fw mr-2 text-gray-400"></i>Form 2 (Billing Details)</a><a class="dropdown-item" href="#"><i class="fas fa-file-download fa-sm fa-fw mr-2 text-gray-400"></i>Form 3 (Billing Details)</a>
+                                                  <h6 class="dropdown-header">SELECT FORM TO DOWNLOAD:</h6>
+                                                  <a class="dropdown-item" href="#"><i class="fas fa-file-download fa-sm fa-fw mr-2 text-gray-400"></i> Form 1 (Billing Summary)</a>
+                                                  <a class="dropdown-item" href="#"><i class="fas fa-file-download fa-sm fa-fw mr-2 text-gray-400"></i> Form 2 (Billing Details)</a>
+                                                  <a class="dropdown-item" href="#"><i class="fas fa-file-download fa-sm fa-fw mr-2 text-gray-400"></i> Form 3 (Billing Details)</a>
                                                 </div>
-                                            </div>
+                                              </div>
                                         </div>
                                     </div>
                                     <div id="show_summary_for_billing"
