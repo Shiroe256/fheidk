0da64b77cebaa5d0564de07ae56e525eb2f30b0d
migsyyy <m.gelorenzo@gmail.com>
2023-10-17 13:44:44 +0800

    format ng total and stuff

--- resources/views/modals/studentfees.blade.php
+++ resources/views/modals/studentfees.blade.php
@@ -7,57 +7,63 @@
             <div class="modal-body">
                 <h2 id="studfee_stud_name"></h2>
                 <h3 id="studfee_enrollment_info"></h3>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Entrance and Admission Fees</td>
-                            <td id="studfee_entrance_and_admission_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Athletic Fees</td>
-                            <td id="studfee_athletic_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Computer Fees</td>
-                            <td id="studfee_computer_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Cultural Fees</td>
-                            <td id="studfee_cultural_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Development Fees</td>
-                            <td id="studfee_development_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Guidance Fees</td>
-                            <td id="studfee_guidance_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Handbook Fees</td>
-                            <td id="studfee_handbook_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Laboratory Fees</td>
-                            <td id="studfee_laboratory_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Library Fees</td>
-                            <td id="studfee_library_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Medical and Dental Fees</td>
-                            <td id="studfee_medical_and_dental_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>Restration Fees</td>
-                            <td id="studfee_registration_fee"></td>
-                        </tr>
-                        <tr>
-                            <td>School ID</td>
-                            <td id="studfee_school_id_fee"></td>
-                        </tr>
-                        <hr>
+                <div class="row">
+                    <div class="col-6">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Entrance and Admission Fees</td>
+                                    <td id="studfee_entrance_and_admission_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Athletic Fees</td>
+                                    <td id="studfee_athletic_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Computer Fees</td>
+                                    <td id="studfee_computer_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Cultural Fees</td>
+                                    <td id="studfee_cultural_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Development Fees</td>
+                                    <td id="studfee_development_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Guidance Fees</td>
+                                    <td id="studfee_guidance_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Handbook Fees</td>
+                                    <td id="studfee_handbook_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Laboratory Fees</td>
+                                    <td id="studfee_laboratory_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Library Fees</td>
+                                    <td id="studfee_library_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Medical and Dental Fees</td>
+                                    <td id="studfee_medical_and_dental_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>Restration Fees</td>
+                                    <td id="studfee_registration_fee"></td>
+                                </tr>
+                                <tr>
+                                    <td>School ID</td>
+                                    <td id="studfee_school_id_fee"></td>
+                                </tr>
+                                <hr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-6">
                         <table>
                             <tr>
                                 <td>Tuition Fees</td>
@@ -68,14 +74,18 @@
                                 <td id="studfee_nstp_fee"></td>
                             </tr>
                             <tr>
-                                <td>Total Fees</td>
+                                <h3>
+                                    <td>Total Fees</td>
+                                </h3>
                                 <strong>
-                                    <td id="studfee_total_fee"></td>
+                                    <h3>
+                                        <td id="studfee_total_fee"></td>
+                                    </h3>
                                 </strong>
                             </tr>
                         </table>
-                    </tbody>
-                </table>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button id="btn_closefees" class="btn btn-light card-button-style" type="button">Close</button>
