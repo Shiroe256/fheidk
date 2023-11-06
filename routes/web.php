fdccd1b687965e2e36073da2e4bb1130de6f5d09
migsyyy <m.gelorenzo@gmail.com>
2023-11-06 16:50:53 +0800

    try

--- routes/web.php
+++ routes/web.php
@@ -50,12 +50,12 @@ Route::put('/new-billing', [BillingController::class, 'newBilling'])->name('newB
 Route::put('/save-settings', [BillingController::class, 'saveSettings'])->name('saveSettings');
 //Billing routes
 Route::get('/billings', [BillingController::class, 'billingList'])->name('billings');
-Route::get('/billings/{ref_no?}', [BillingController::class, 'billingmanagementpage']);
-Route::get('/billings/{ref_no?}/attachments', [BillingController::class, 'billingmanagementattachments'])->name('billingmanagementattachments');
-Route::get('/billings/{ref_no?}/attachments/form1', [BillingController::class, 'generatePDFForm1']);
-Route::get('/billings/{ref_no?}/attachments/form2', [BillingController::class, 'generatePDFForm2']);
-Route::get('/billings/{ref_no?}/attachments/form3', [BillingController::class, 'generatePDFForm3']);
+Route::get('/billings/{ref_no}/attachments/form1', [BillingController::class, 'generatePDFForm1']);
+Route::get('/billings/{ref_no}/attachments/form2', [BillingController::class, 'generatePDFForm2']);
+Route::get('/billings/{ref_no}/attachments/form3', [BillingController::class, 'generatePDFForm3']);
+Route::get('/billings/{ref_no}/attachments', [BillingController::class, 'billingmanagementattachments'])->name('billingmanagementattachments');
 Route::get('/billings/{ref_no}/settings', [BillingController::class, 'getBillingSettings'])->name('getBillingSettings');
+Route::get('/billings/{ref_no?}', [BillingController::class, 'billingmanagementpage']);
 Route::post('/submitbilling', [BillingController::class, 'submitbilling'])->name('submitbilling');
 Route::get('/fees/{uid}', [BillingController::class, 'fetchStudentFees'])->name('fetchStudentFees'); //student fees
 Route::get('registers', 'App\Http\Controllers\Pagescontroller@registers')->name('registers');
