313898e492b072fe9a0d5fc9eebe98319cd77082
unifastph <ictunit.unifast@ched.gov.ph>
2024-02-05 14:31:46 +0800

    pwede undefined sa pass or failed

--- public/js/student_crud.js
+++ public/js/student_crud.js
@@ -482,7 +482,7 @@ function validateFields(data) {
     if (stud['acad_u'] > 40 || stud['acad_u'] === undefined) error.push('Invalid Academic unit value. The academic unit only accepts between 1 to 40.');
     if (stud['nstp_u'] > 40 || stud['nstp_u'] === undefined) error.push('Invalid NSTP unit value. The NSTP unit only accepts between 1 to 40.');
     if (stud['exams'] > 40 || stud['exams'] === undefined) error.push('Invalid exams value. Please input only how many times the student has taken the exam in whole numbers.');
-    if (stud['exam_result'] != 'PASSED' && stud['exam_result'] != 'FAILED') error.push('Invalid exam result. Please enter only PASSED or FAILED');
+    if (stud['exam_result'] != 'PASSED' && stud['exam_result'] != 'FAILED' || stud['exam_result'] === undefined) error.push('Invalid exam result. Please enter only PASSED or FAILED');
     // stud['remarks']
 
     if (error.length > 0) errors.push({ seq_no: row, error: error });
