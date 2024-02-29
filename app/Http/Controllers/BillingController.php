1bb94496bbc6dcf4406bd6c8126e40cafde9d56e
unifastph <ictunit.unifast@ched.gov.ph>
2024-02-29 15:46:41 +0800

    settings

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -1168,7 +1168,23 @@ SUM(
                         ->on('tbl_other_school_fees.hei_uii', '=', DB::raw($hei_uii))
                         ->on('tbl_other_school_fees.semester', '=', 'students_sub.semester')
                         ->on('tbl_other_school_fees.year_level', '=', 'students_sub.year_level');
-                });
+                })->leftJoin('tbl_billing_settings', function ($join) {
+                    $join->on('tbl_billing_settings.bs_osf_uid', '=', 'tbl_other_school_fees.uid')
+                        ->on('tbl_billing_settings.bs_reference_no', '=', 'students_sub.reference_no');
+                })
+                ->leftJoin('tbl_billing_stud_settings', function ($join) {
+                    $join->on('tbl_billing_stud_settings.bs_reference_no', '=', 'students_sub.reference_no')
+                        ->on('tbl_billing_stud_settings.bs_student', '=', 'students_sub.uid')
+                        ->on('tbl_billing_settings.bs_osf_uid', '=', 'tbl_billing_stud_settings.bs_osf_uid');
+                })
+                ->where(function ($query) {
+                    $query->where('tbl_billing_stud_settings.bs_status', '=', 1)
+                        ->where('tbl_billing_settings.bs_status', '=', 1)
+                        ->orWhere(function ($query) {
+                            $query->whereNull('tbl_billing_stud_settings.bs_status')
+                                ->where('tbl_billing_settings.bs_status', '=', 1);
+                        });
+                });;
         } else {
             $students = DB::table(DB::raw("({$students_sub->toSql()}) AS students_sub"))
                 ->mergeBindings($students_sub)
@@ -1182,7 +1198,23 @@ SUM(
                         ->on('tbl_other_school_fees.semester', '=', 'students_sub.semester')
                         ->on('tbl_other_school_fees.year_level', '=', 'students_sub.year_level')
                         ->on('tbl_other_school_fees.form', '=', DB::raw($form));
-                });
+                })->leftJoin('tbl_billing_settings', function ($join) {
+                    $join->on('tbl_billing_settings.bs_osf_uid', '=', 'tbl_other_school_fees.uid')
+                        ->on('tbl_billing_settings.bs_reference_no', '=', 'students_sub.reference_no');
+                })
+                ->leftJoin('tbl_billing_stud_settings', function ($join) {
+                    $join->on('tbl_billing_stud_settings.bs_reference_no', '=', 'students_sub.reference_no')
+                        ->on('tbl_billing_stud_settings.bs_student', '=', 'students_sub.uid')
+                        ->on('tbl_billing_settings.bs_osf_uid', '=', 'tbl_billing_stud_settings.bs_osf_uid');
+                })
+                ->where(function ($query) {
+                    $query->where('tbl_billing_stud_settings.bs_status', '=', 1)
+                        ->where('tbl_billing_settings.bs_status', '=', 1)
+                        ->orWhere(function ($query) {
+                            $query->whereNull('tbl_billing_stud_settings.bs_status')
+                                ->where('tbl_billing_settings.bs_status', '=', 1);
+                        });
+                });;
         }
 
         return $students;
