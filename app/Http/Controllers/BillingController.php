961353d4c735ef09aa7e5f84f19de0006a89216d
unifastph <ictunit.unifast@ched.gov.ph>
2024-03-11 16:37:36 +0800

    try

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -1040,30 +1040,33 @@ SUM(
         $student = DB::table(DB::raw("({$students_sub->toSql()}) AS students_sub"))
             ->mergeBindings($students_sub)
             ->select(
-                'students_sub.*',
+                'students_sub.uid',
+                'students_sub.app_id',
+                'students_sub.hei_name',
+                'students_sub.hei_uii',
+                'students_sub.stud_lname',
+                'students_sub.stud_fname',
+                'students_sub.stud_mname',
+                'students_sub.stud_ext_name',
+                'students_sub.fhe_award_no',
+                'students_sub.degree_program',
+                'students_sub.academic_unit',
+                'students_sub.lab_unit',
+                'students_sub.comp_lab_unit',
+                'students_sub.nstp_unit',
+                'students_sub.total_exam_taken',
+                'students_sub.exam_result',
+                'students_sub.semester',
+                'students_sub.year_level',
+                'students_sub.remarks',
+                'students_sub.stud_status',
                 DB::raw($this->carlo_columns)
             )
-            ->leftJoin('tbl_other_school_fees', function ($join) use ($hei_uii, $form) {
+            ->leftJoin('tbl_other_school_fees', function ($join) use ($hei_uii) {
                 $join->on('tbl_other_school_fees.course_enrolled', '=', 'students_sub.degree_program')
                     ->on('tbl_other_school_fees.hei_uii', '=', DB::raw($hei_uii))
                     ->on('tbl_other_school_fees.semester', '=', 'students_sub.semester')
                     ->on('tbl_other_school_fees.year_level', '=', 'students_sub.year_level');
-            })->leftJoin('tbl_billing_settings', function ($join) {
-                $join->on('tbl_billing_settings.bs_osf_uid', '=', 'tbl_other_school_fees.uid')
-                    ->on('tbl_billing_settings.bs_reference_no', '=', 'students_sub.reference_no');
-            })
-            ->leftJoin('tbl_billing_stud_settings', function ($join) {
-                $join->on('tbl_billing_stud_settings.bs_reference_no', '=', 'students_sub.reference_no')
-                    ->on('tbl_billing_stud_settings.bs_student', '=', 'students_sub.uid')
-                    ->on('tbl_billing_settings.bs_osf_uid', '=', 'tbl_billing_stud_settings.bs_osf_uid');
-            })
-            ->where(function ($query) {
-                $query->where('tbl_billing_stud_settings.bs_status', '=', 1)
-                    ->where('tbl_billing_settings.bs_status', '=', 1)
-                    ->orWhere(function ($query) {
-                        $query->whereNull('tbl_billing_stud_settings.bs_status')
-                            ->where('tbl_billing_settings.bs_status', '=', 1);
-                    });
             })
             ->get()->toArray();
         $f = new NumberFormatter('en', NumberFormatter::ORDINAL);
