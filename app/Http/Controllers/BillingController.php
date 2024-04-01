5ee8f8dfc3aa5fa0fc359c6957165c3f34c96be7
unifastph <ictunit.unifast@ched.gov.ph>
2024-04-01 16:25:38 +0800

    ayan pota

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -410,9 +410,6 @@ class BillingController extends Controller
                                                                 (
                                                                     CASE
                                                                         WHEN(
-                                                                            `students_sub`.`lab_unit` + `students_sub`.`comp_lab_unit` + `students_sub`.`academic_unit` + `students_sub`.`nstp_unit` = 0
-                                                                        ) THEN 0
-                                                                        WHEN(
                                                                             `tbl_other_school_fees`.`type_of_fee` = 'entrance'
                                                                         ) THEN `tbl_other_school_fees`.`amount`
                                                                         ELSE 0
@@ -422,9 +419,6 @@ class BillingController extends Controller
                                                         ) + SUM(
                                                             (
                                                                 CASE
-                                                                    WHEN(
-                                                                        `students_sub`.`lab_unit` + `students_sub`.`comp_lab_unit` + `students_sub`.`academic_unit` + `students_sub`.`nstp_unit` = 0
-                                                                    ) THEN 0
                                                                     WHEN(
                                                                         `tbl_other_school_fees`.`type_of_fee` = 'admission'
                                                                     ) THEN `tbl_other_school_fees`.`amount`
