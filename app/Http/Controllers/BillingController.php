184deed5cd82fd46faf2d06f956cd27ff3772db7
unifastph <ictunit.unifast@ched.gov.ph>
2024-04-01 14:40:50 +0800

    eto

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -1339,7 +1339,11 @@ SUM(
         $form3_fees = $this->joinStudentFees($form3_stud, 3)->groupBy('students_sub.reference_no')->first();
 
         $data['form2_total'] = (int)$form2_fees->total_fee;
-        $data['form3_total'] = (int)$form3_fees->total_fee;
+        if(count($form3_fees) < 1){
+            $data['form3_total'] = 0;
+        } else {
+            $data['form3_total'] = (int)$form3_fees->total_fee;
+        }
         $data['total_fee'] = (int)$form3_fees->total_fee + (int)$form2_fees->total_fee;
         $data['hei_name'] = $form2_fees->hei_name;
         $data['hei_uii'] = $form2_fees->hei_uii;
