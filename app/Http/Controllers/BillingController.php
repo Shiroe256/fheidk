46e72107a4f1760e24a4a962da538d1a46b765a2
unifastph <ictunit.unifast@ched.gov.ph>
2024-04-02 16:28:14 +0800

    try

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -1415,8 +1415,8 @@ class BillingController extends Controller
                     $join->on('tbl_other_school_fees.course_enrolled', '=', 'students_sub.degree_program')
                         ->on('tbl_other_school_fees.hei_uii', '=', DB::raw($hei_uii))
                         ->on('tbl_other_school_fees.coverage', '=', DB::raw("'per new student'")) // Additional condition for transferees
-                        ->on('tbl_other_school_fees.form', '=', DB::raw($form))
-                        ->where('students_sub.transferee', '=', 'yes'); // Only for transferees
+                        ->on('tbl_other_school_fees.form', '=', DB::raw($form));
+                        // ->where('students_sub.transferee', '=', 'yes'); // Only for transferees
                 })
                 ->leftJoin('tbl_billing_settings', function ($join) {
                     $join->on('tbl_billing_settings.bs_osf_uid', '=', 'tbl_other_school_fees.uid')
