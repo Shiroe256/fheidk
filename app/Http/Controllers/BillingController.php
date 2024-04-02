0ef3f963f3e98504150fb2b1d474f7f70fa9646e
unifastph <ictunit.unifast@ched.gov.ph>
2024-04-02 17:18:08 +0800

    try

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -1466,8 +1466,10 @@ class BillingController extends Controller
                                 ->where('tbl_billing_settings.bs_status', '=', 1);
                         });
                 });
-            $students = $students_fees->union($transferee_fees->whereNotNull('students_sub.uid'));
-            // $students = $students_fees;
+            if ($transferee_fees->count() < 1) {
+                $students = $students_fees;
+            } else
+                $students = $students_fees->union($transferee_fees->whereNotNull('students_sub.uid'));
             // $students = $transferee_fees;
         }
         if ($form == 3) {
