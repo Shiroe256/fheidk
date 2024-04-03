deb03950c5066c9070599612830ea7d2bb130e84
unifastph <ictunit.unifast@ched.gov.ph>
2024-04-03 14:17:37 +0800

    try

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -1542,7 +1542,7 @@ class BillingController extends Controller
             "draw" => $request->draw,
             "recordsTotal" => count($students),
             "recordsFiltered" => $total,
-            "data" => json_encode($students)
+            "data" => $students[0]
         ]);
     }
     public function fetchTempApplicantJSON(Request $request)
