1bbb592414f6f3c504682875f85504e6d599b1ae
unifastph <ictunit.unifast@ched.gov.ph>
2024-04-04 17:27:58 +0800

    . pala

--- app/Http/Middleware/validateTempStudentFields.php
+++ app/Http/Middleware/validateTempStudentFields.php
@@ -40,8 +40,8 @@ class validateTempStudentFields
             if (!in_array($tempstudent->degree_course_id, $courses))
                 $courses_str = '';
             foreach ($courses as $course)
-                $courses_str += ' ' + $course;
-            return response('Invalid Course in Row ' . $key + 1 . '. Only choose courses in your template or the ones submitted in your certified TOSF. Courses:' + $courses_str, 400);
+                $courses_str = $courses_str . ' ' . $course;
+            return response('Invalid Course in Row ' . $key + 1 . '. Only choose courses in your template or the ones submitted in your certified TOSF. Courses:' . $courses_str, 400);
             $error = $this->validateTempStudentFields($tempstudent);
             if (count($error) > 0) return response('Invalid Input in ' . array_keys($error)[0] . ' in Row ' . $key + 1, 400);
         }
