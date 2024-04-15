02efa15d3215628be58daca921811edbc0a56f9f
unifastph <ictunit.unifast@ched.gov.ph>
2024-04-15 15:20:49 +0800

    courses

--- app/Http/Middleware/validateTempStudentFields.php
+++ app/Http/Middleware/validateTempStudentFields.php
@@ -34,11 +34,11 @@ class validateTempStudentFields
 
         $hei_uii = Auth::user()->hei_uii;
         $tempstudents =  json_decode($request->payload); //json decode into array (the second parameter)
-        $courses_raw = array_column(OtherSchoolFees::select('course_enrolled')->where('hei_uii', $hei_uii)->groupBy('course_enrolled')->get()->toArray(), 'course_enrolled');
+        $courses = array_column(OtherSchoolFees::select('course_enrolled')->where('hei_uii', $hei_uii)->groupBy('course_enrolled')->get()->toArray(), 'course_enrolled');
         if (count($tempstudents) < 1) return response('Invalid', 400);
-        foreach ($courses_raw as $crs) {
-            $courses[] = strtoupper($crs);
-        }
+        // foreach ($courses_raw as $crs) {
+        //     $courses[] = strtoupper($crs);
+        // }
         foreach ($tempstudents as $key => $tempstudent) {
             $courses_str = '';
             if (!in_array(strtoupper($tempstudent->degree_course_id), $courses))
