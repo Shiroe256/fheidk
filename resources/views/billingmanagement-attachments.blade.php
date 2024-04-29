cd2157b421d460208f7fa0353cc3e9a91929974f
Carlo Molina <cmolina.unifast@ched.gov.ph>
2024-04-29 11:32:03 +0800

    Update billingmanagement-attachments.blade.php

--- resources/views/billingmanagement-attachments.blade.php
+++ resources/views/billingmanagement-attachments.blade.php
@@ -339,6 +339,56 @@
                                             </tr>
                                             <tr>
                                                 <td class="text-center">6</td>
+                                                <td class="text-left">Certificate of Registration of Students (CORs)
+                                                </td>
+                                                <td class="text-left"> <a href="{{ $billings->cor_link }}"
+                                                        target="_blank">{{ $billings->cor_link }}</a></td>
+                                                <td class="text-center">
+                                                    @if ($billings->cor_status == 0)
+                                                        <span class="badge badge-pill badge-secondary input-style">No
+                                                            Attachment</span>
+                                                    @elseif ($billings->cor_status == 1)
+                                                        <span class="badge badge-pill badge-warning input-style">For
+                                                            Review</span>
+                                                    @elseif ($billings->cor_status == 2)
+                                                        <span
+                                                            class="badge badge-pill badge-success input-style">Approved
+                                                            by UniFAST Billing Unit</span>
+                                                    @elseif ($billings->cor_status == 3)
+                                                        <span
+                                                            class="badge badge-pill badge-danger input-style">Rejected
+                                                            by UniFAST Billing Unit</span>
+                                                    @elseif ($billings->cor_status == 4)
+                                                        <span
+                                                            class="badge badge-pill badge-success input-style">Approved
+                                                            by CHED-AFMS</span>
+                                                    @elseif ($billings->cor_status == 5)
+                                                        <span
+                                                            class="badge badge-pill badge-danger input-style">Rejected
+                                                            by CHED-AFMS</span>
+                                                    @endif
+                                                </td>
+                                                <td class="text-center"></td>
+                                                <td class="text-center">
+                                                    <div class="btn-group btn-group-sm" role="group">
+                                                        <button id="{{ $billings->reference_no }}"
+                                                            name="btn_link_cor"
+                                                            class="btn_link_cor btn btn-outline-info"
+                                                            data-bs-toggle="modal" data-bs-tooltip=""
+                                                            data-placement="bottom" type="button"
+                                                            title="Attach link for student cor's"
+                                                            data-bs-target="#mod_upload_link_cor"><i
+                                                                class="fas fa-paperclip"></i></button>
+                                                        <a class="btn btn-outline-info" role="button"
+                                                            data-bs-tooltip="" data-placement="bottom"
+                                                            title="View billing submission"
+                                                            href="{{ $billings->cor_link }}" target="_blank"><i
+                                                                class="far fa-eye"></i></a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td class="text-center">7</td>
                                                 <td class="text-left">Bank Certification of the HEI Certified by the
                                                     HEI
                                                 </td>
@@ -389,7 +439,7 @@
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td class="text-center">7</td>
+                                                <td class="text-center">8</td>
                                                 <td class="text-left">Bank Certification of the HEI Certified by the
                                                     Bank
                                                 </td>
